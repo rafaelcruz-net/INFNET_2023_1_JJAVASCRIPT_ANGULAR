1c55a4ce6173e6d81e86f49795edc8b278f04380
Rafael Cruz <rafael.cruz@prof.infnet.edu.br>
2023-06-27 08:29:48 -0300

    adicionando exemplo angular diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './model/product';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'entendendo-diretivas';
+  public products = new Array<Product>();
+
+  public constructor() {
+    let produto = {
+      name: "Geladeira",
+      price: 1200.99,
+      productCode: "123AA",
+      releaseDate: new Date()
+    } as Product;
+
+    this.products.push(produto);
+
+    let produto2 = {
+      name: "Fogao",
+      price: 699.99,
+      productCode: "123BB",
+      releaseDate: new Date()
+    } as Product;
+
+    this.products.push(produto2);
+
+  }
 }
---
+++ src/app/model/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  name: string;
+  price: Number;
+  releaseDate: Date;
+  productCode: string;
+}
